0c91abc88d11f71c0dfba42703647c250c82d911
bernardopiane <bernardopiane@users.noreply.github.com>
2020-07-21 14:04:47 -0300

    Updates

---
+++ precache-manifest.17020058eb44dc180df36d547e467c8c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d5454b97d668380135ecb62d5a0eee4",
+    "url": "/appo/index.html"
+  },
+  {
+    "revision": "392d5f5710048b25d4b9",
+    "url": "/appo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fbf11ce093d05481e0ab",
+    "url": "/appo/static/css/main.38fff912.chunk.css"
+  },
+  {
+    "revision": "392d5f5710048b25d4b9",
+    "url": "/appo/static/js/2.24058adf.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/appo/static/js/2.24058adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbf11ce093d05481e0ab",
+    "url": "/appo/static/js/main.0647f7d2.chunk.js"
+  },
+  {
+    "revision": "0128e898adb8768ef43a",
+    "url": "/appo/static/js/runtime-main.38c03fa0.js"
+  },
+  {
+    "revision": "7f47ec10bccdde10fe8fd960257f7b2a",
+    "url": "/appo/static/media/2phones.jpg.7f47ec10.png"
+  },
+  {
+    "revision": "25edae9e15861f7502b6515b0b41f101",
+    "url": "/appo/static/media/Code Window.25edae9e.png"
+  },
+  {
+    "revision": "d3564b3b08320a9288c9327a793efd46",
+    "url": "/appo/static/media/Screen.d3564b3b.png"
+  },
+  {
+    "revision": "3b8843e8f97f7e76b1b382581754456c",
+    "url": "/appo/static/media/girl.3b8843e8.mp4"
+  }
+]);
